6b265bdee61f628d4095b658ae5d82c726567125
Kavindu Bimsara <34527634+kbimsara@users.noreply.github.com>
2024-12-24 09:37:47 +0530

    Publish App

---
+++ lib/components/bookCard.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class BookCard extends StatelessWidget {
+  final String img;
+  final String title;
+  final String author;
+  final String price;
+
+  const BookCard({
+    super.key,
+    required this.img,
+    required this.title,
+    required this.author,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.lightBlueAccent,
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          children: [
+            Image.asset(
+              img,
+              // width: 70,
+              height: 180,
+              // fit: BoxFit.cover,
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    author,
+                    style: const TextStyle(fontSize: 16, color: Colors.black54),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Rs ' + price,
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.shopping_cart),
+                    label: const Text('Add to cart'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      elevation: 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
